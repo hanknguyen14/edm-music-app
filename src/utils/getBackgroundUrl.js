7f817e8a061edf73648a470a10e34fc69399e79e
Hung M. Nguyen <hung.manh.nguyen@codeenginestudio.com>
2020-04-16 21:29:16 +0700

    Update engines config

--- src/utils/getBackgroundUrl.js
+++ src/utils/getBackgroundUrl.js
@@ -1,14 +1,13 @@
 const BACKGROUND_FOLDER = 'backgrounds';
 const BACKGROUND_URLS = [
   'background1.jpg',
-  'background2.jpg',
+  'background2.jpeg',
   'background3.jpg',
   'background4.jpg',
   'background5.jpg',
   'background6.jpg',
   'background7.jpg',
   'background8.jpeg',
-  'background9.jpeg',
 ];
 const getRandomBackgroundUrl = () => {
   const backgroundIndex = Math.floor(Math.random() * BACKGROUND_URLS.length);
