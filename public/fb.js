a870e928ee17e4de7fd59fed3472caff4ed3072d
Hung M. Nguyen <hung.manh.nguyen@codeenginestudio.com>
2020-04-20 16:26:25 +0700

    Test facebook sdk

---
+++ public/fb.js
@@ -0,0 +1,30 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '216791979419969',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v6.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     let js; const fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '821324588360187');
+    fbq('track', 'PageView');
--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect, useRef } from 'react';
 import { BaseCSS } from 'styled-bootstrap-grid';
 import StarCover from 'components/StarCover';
 import getRandomBackgroundUrl from 'utils/getBackgroundUrl';
@@ -7,9 +7,16 @@ import StyledApp from './StyledApp.js';
 const Header = lazy(() => import('components/Header'));
 
 function App() {
+  const instance = useRef(null);
+  useEffect(() => {
+    const scriptTag = document.createElement('script');
+    scriptTag.src = 'fb.js';
+    instance.current.appendChild(scriptTag);
+  }, []);
   const backgroundUrl = getRandomBackgroundUrl();
   return (
     <StyledApp fluid backgroundUrl={backgroundUrl}>
+      <div ref={instance} />
       <Suspense fallback={<div>Loading...</div>}>
         <Header />
         <StarCover />
